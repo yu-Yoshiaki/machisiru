dd2793c8d50884cad294c300da629f2ae2fa730c
yu-Yoshiaki <32384795+yu-Yoshiaki@users.noreply.github.com>
2021-06-19 23:59:43 +0900

    Update microcms.js

--- src/libs/microcms.js
+++ src/libs/microcms.js
@@ -1,6 +1,6 @@
 import { createClient } from "microcms-js-sdk";
 
 export const client = createClient({
-  serviceDomain: "mytwitter",
-  apiKey: "87bf07d4-71de-4f79-a791-f2329f955341",
+  serviceDomain: process.env.MICROCMS_DOMAIN,
+  apiKey: process.env.MICROCMS_API_KEY,
 });
