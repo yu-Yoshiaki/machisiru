c045abd7cdffe47af4dd62a98d622f88d1939ba7
yoshiaki44 <yo44ch@gmail.com>
2021-06-21 23:35:23 +0900

    Delete /news/index.tsx

--- src/components/articleListTabView.tsx
+++ src/components/articleListTabView.tsx
@@ -9,7 +9,7 @@ type ArticleListTabView = {
 
 export const ArticleListTabView: VFC<ArticleListTabView> = (props) => {
   return (
-    <div className="md:inline-block relative pb-5 mx-5 mb-3 hover:bg-white md:w-3/10">
+    <div className="md:inline-block relative pb-5 mx-5 mb-3 md:w-3/10 hover:bg-white">
       <Image src={props.image} width={500} height={300} className="rounded-md shadow-lg" alt="No Image" />
       {/* <h1 className="absolute top-1 left-2 text-lg text-white" id="title">
         <strong>サムネイルメッセージ</strong>
