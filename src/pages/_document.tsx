904be2e82f608947a2d57fee57a205955a8ed368
yoshiaki44 <yo44ch@gmail.com>
2021-09-19 23:13:30 +0900

    Add AdSense

---
+++ src/pages/_document.tsx
@@ -0,0 +1,37 @@
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="ja">
+        <Head>
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1033411179198037"
+            crossOrigin="anonymous"
+          ></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1033411179198037"
+            crossOrigin="anonymous"
+          ></script>
+          <ins
+            // eslint-disable-next-line tailwindcss/no-custom-classname
+            className="adsbygoogle"
+            data-ad-client={process.env.DATA_AD_CLIENT}
+            data-ad-slot={process.env.DATA_AD_SLOT}
+            data-ad-format="auto"
+            data-full-width-responsive="true"
+          ></ins>
+          <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
