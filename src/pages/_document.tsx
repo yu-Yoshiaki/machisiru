d16ec0b5f6c087155f44fb46242fafcfb1c395f3
yoshiaki44 <yo44ch@gmail.com>
2021-09-19 23:53:11 +0900

    Add AdSense

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,16 +1,9 @@
-import Document, { Head, Html, Main, NextScript } from "next/document";
+import Document, { Html, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="ja">
-        <Head>
-          <script
-            async
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1033411179198037"
-            crossOrigin="anonymous"
-          ></script>
-        </Head>
         <body>
           <Main />
           <NextScript />
