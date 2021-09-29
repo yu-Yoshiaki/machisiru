00cef493ec730b2152e77810789bb01681e7bf76
yoshiaki44 <yo44ch@gmail.com>
2021-09-29 23:51:18 +0900

    chagne pan.tsx

--- src/components/pan.tsx
+++ src/components/pan.tsx
@@ -4,11 +4,13 @@ import type { VFC } from "react";
 
 export const Pan: VFC<{ title: string }> = (props) => {
   return (
-    <nav className="flex overflow-scroll md:overflow-auto md:col-span-3 md:py-4 px-2 md:px-4 space-x-2 md:space-x-4 h-5 text-sm md:text-base bg-white">
-      <Link href="/">
-        <a className="font-bold">HOME</a>
-      </Link>
-      <p className="md:before:mr-2 before:content-['▶︎']">{props.title}</p>
-    </nav>
+    <div className="md:col-span-3 space-x-2 md:space-x-4 bg-white">
+      <nav className="flex overflow-scroll md:overflow-auto md:py-4 px-2 md:px-4 mx-auto w-[95%] md:w-auto text-sm md:text-base whitespace-nowrap bg-gray-50 md:bg-white scrollbar-hide">
+        <Link href="/">
+          <a className="font-bold">HOME</a>
+        </Link>
+        <p className="md:before:mr-2 before:content-['▶︎']">{props.title}</p>
+      </nav>
+    </div>
   );
 };
