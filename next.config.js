745486d48522e04b30a2cd1b514d0efafe6fe7cc
yoshiaki44 <yo44ch@gmail.com>
2021-10-19 06:46:00 +0900

    routing変更。.tsx→.page.tsx　等へ

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
   future: { webpack5: true, strictPostcssConfiguration: true },
   i18n: { locales: ["ja"], defaultLocale: "ja" },
   reactStrictMode: true,
