ed4f6c61072908270f64a98803cef9b86ede3c8c
yoshiaki44 <yo44ch@gmail.com>
2021-10-19 20:49:10 +0900

    コンポーネント名の最適化と、いらない物をとりあえず削除。後に追加するかも。。。

--- src/pages/news/microcms/ImageCustomField.tsx
+++ src/pages/news/microcms/ImageCustomField.tsx
@@ -1,14 +1,11 @@
 import Image from "next/image";
 import type { VFC } from "react";
-import type { MicroCMSField } from "src/types/microcms";
+import type { MicroCMSCustomField, MicroCMSField } from "src/pages/news/types";
 
 //カスタムフィールド「画像」の型
-type McImage = {
-  image: MicroCMSField["image"];
-  alt: string;
-};
+type ImageCustomField = MicroCMSCustomField<"image", { image: MicroCMSField["image"]; alt: string }>;
 
-export const McImage: VFC<McImage> = (props) => {
+export const ImageCustomField: VFC<ImageCustomField> = (props) => {
   return (
     <div className="w-full ">
       <Image src={props.image.url} width={600} height={600} layout={"responsive"} alt={props.alt} className="mx-auto" />
