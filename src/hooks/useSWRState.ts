8aa1ad6a99a4794f70e7cdbed85c8aec150151a9
yoshiaki44 <yo44ch@gmail.com>
2022-01-15 19:43:05 +0900

    抽象化を変更

--- src/hooks/useSWRState.ts
+++ src/hooks/useSWRState.ts
@@ -1,7 +1,7 @@
 import useSWR from "swr";
 
-export const useSWRState = (key: string, fallback?: any) => {
-  const { data, mutate } = useSWR(key, { fallback });
+export const useSWRState = (key: string, fallbackData?: any) => {
+  const { data, mutate } = useSWR(key, null, { fallbackData });
 
   return [data, mutate];
 };
