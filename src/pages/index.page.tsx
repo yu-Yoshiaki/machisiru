b11e782a2405fb791a76b4a6a1754e77c1c24ddd
yoshiaki44 <yo44ch@gmail.com>
2022-01-15 19:34:07 +0900

    newsへ飛ばす。

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,8 +1,16 @@
 import type { CustomNextPage } from "next";
+import { useRouter } from "next/dist/client/router";
+import { useEffect } from "react";
 import { Layout } from "src/Layout";
 
 const Index: CustomNextPage = () => {
-  return <div>ようこそ</div>;
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/news");
+  }, [router]);
+
+  return <div></div>;
 };
 
 Index.getLayout = Layout;
