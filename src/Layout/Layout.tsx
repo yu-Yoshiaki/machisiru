d11ef6089f9cf5e3b2e34494df2e9a121471a553
yoshiaki44 <yo44ch@gmail.com>
2022-01-18 22:40:52 +0900

    デザイン微調整

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -6,10 +6,10 @@ import { Side } from "./Side";
 export const Layout: CustomLayout = (page) => {
   return (
     <>
-      <div className="md:grid grid-rows-[auto,1fr,auto] gap-y-8 bg-gray-100 ">
+      <div className="md:grid grid-rows-[auto,1fr] gap-y-8 font-mono bg-gray-100 ">
         <Header />
-        <main className="md:mx-auto md:w-5/6">
-          <div className="md:grid grid-cols-[70%,1fr] gap-y-4">
+        <main className="pt-3 pb-10 md:mx-auto md:w-5/6">
+          <div className="md:grid grid-cols-[auto,30%] gap-x-4 gap-y-4">
             {page}
             <Side />
           </div>
