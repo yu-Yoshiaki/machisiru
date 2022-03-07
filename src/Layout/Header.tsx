55355f82a6aa8b66efa59eb668680b048b88375e
yu-Yoshiaki <32384795+yu-Yoshiaki@users.noreply.github.com>
2022-03-07 17:47:28 +0900

    デザイン変更＆ヘッダーの固定化

--- src/Layout/Header.tsx
+++ src/Layout/Header.tsx
@@ -6,17 +6,14 @@ import { Navigation } from "./Navigation";
 
 export const Header: VFC = () => {
   return (
-    <header className="py-3 h-20 bg-white">
-      <h1 className="w-full text-3xl font-bold text-center">
+    <header className="fixed top-0 z-20 bg-white w-full h-[100px]">
+      <h1 className="font-bold flex items-center h-full pl-8">
         <Link href="/">
-          <a>
-            FIND <span className="text-red-500">NEXT</span>
+          <a className="text-3xl">
+            FIND<span className="text-red-500">NEXT</span>
           </a>
         </Link>
       </h1>
-      <h2 className="md:p-0 px-2 pb-4 md:pb-0 w-full text-sm md:text-base text-center">ー新しい次が見つかるー</h2>
-
-      <Navigation />
     </header>
   );
 };
