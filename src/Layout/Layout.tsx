93d14cba591d6388fa9013b47c8adaefbe8adcf6
yu-Yoshiaki <32384795+yu-Yoshiaki@users.noreply.github.com>
2022-03-08 11:20:55 +0900

    デザイン＆レイアウト変更、微調整

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -8,7 +8,7 @@ export const Layout: CustomLayout = (page) => {
   return (
     <div className="grid grid-rows-[auto,1fr,auto] gap-y-8 font-mono bg-gray-100 justify-center">
         <Header />
-        <main className="grid grid-cols-1 md:grid-cols-[min(500px),250px] gap-x-8 min-h-screen max-w-[1110px] pt-[120px]">
+        <main className="grid grid-cols-1 md:grid-cols-[min(500px),250px] gap-y-8 gap-x-8 min-h-screen max-w-[1110px] pt-[120px]">
           {page}
           <Side />
         </main>
--- src/pages/news/component/Card.tsx
+++ src/pages/news/component/Card.tsx
@@ -11,21 +11,21 @@ type CardProps = {
 
 export const Card: VFC<CardProps> = (props) => {
   return (
-    <article className="pb-7 tracking-wide bg-white hover:bg-gray-200 rounded-md shadow-sm w-[250px]">
+    <article className="   tracking-wide bg-white hover:bg-gray-200 rounded-lg shadow-sm w-[250px] h-[280px]">
       <Link href={`/news/${props.id}`}>
         <a>
-          <div className="p-3">
+          <div>
             <Image
               src={props.image}
-              width={350}
-              height={300}
+              width={200}
+              height={150}
               layout={"responsive"}
               alt="No Image"
-              className="mx-auto"
+              className="rounded-t-lg"
             />
           </div>
           <p className="pt-2 pl-3 text-xs text-left text-gray-600">{props.updatedAt.substring(0, 10)}</p>
-          <h2 className="overflow-hidden p-3 md:h-14 font-bold overflow-ellipsis ">{props.mainTitle}</h2>
+          <h2 className="overflow-hidden px-3 font-bold overflow-ellipsis ">{props.mainTitle}</h2>
         </a>
       </Link>
     </article>
