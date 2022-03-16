e48dbfac8720ca137a1e9326f3896497c418815d
yoshiaki <yoshiosmile68@gmail.com>
2022-03-16 09:17:36 +0900

    プロジェクト名変更

--- src/Layout/Footer.tsx
+++ src/Layout/Footer.tsx
@@ -3,7 +3,7 @@ import type { VFC } from "react";
 export const Footer: VFC = () => {
   return (
     <footer className="hidden md:block text-xl text-center bg-white border-t">
-      <small>&copy; 2021 FIND NEXT</small>
+      <small>&copy; 2021 MACHISIRU</small>
     </footer>
   );
 };
