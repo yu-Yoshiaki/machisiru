28519e7ed7b8edd27267d1cad74bbc58f9caf92b
yoshiaki <yoshiosmile68@gmail.com>
2022-03-17 00:43:10 +0900

    アロー関数形式に変更

--- src/pages/api/test.page.ts
+++ src/pages/api/test.page.ts
@@ -1,7 +1,5 @@
-/* eslint-disable @typescript-eslint/naming-convention */
 import type { NextApiRequest, NextApiResponse } from "next";
 
-// eslint-disable-next-line import/no-default-export
-export default async (_req: NextApiRequest, res: NextApiResponse) => {
+export const A = async (_req: NextApiRequest, res: NextApiResponse) => {
   res.status(200).json({ test: "hello" });
 };
