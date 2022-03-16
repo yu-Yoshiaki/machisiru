a5ca5178865fc0bd7cd3898c050783cf1e37b7f3
yoshiaki <yoshiosmile68@gmail.com>
2022-03-16 23:07:20 +0900

    レイアウト、pxサイズの調整

--- src/Layout/Header.tsx
+++ src/Layout/Header.tsx
@@ -4,7 +4,7 @@ import type { VFC } from "react";
 
 export const Header: VFC = () => {
   return (
-    <header className="fixed top-0 z-20 w-full h-[100px] bg-white">
+    <header className="fixed top-0 z-20 w-full h-[90px] bg-white">
       <h1 className="flex items-center pl-8 h-full font-bold">
         <Link href="/">
           <a className="text-3xl　text-red-500">MACHISIRU /「こんな街に住みたい」が見つかるメディア</a>
--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -6,12 +6,14 @@ import { Side } from "./Side";
 
 export const Layout: CustomLayout = (page) => {
   return (
-    <div className="grid grid-rows-[auto,1fr,auto] gap-y-8 justify-center font-mono bg-gray-100">
+    <div className="font-mono bg-gray-300">
       <Header />
-      <main className="grid grid-cols-1 md:grid-cols-[min(500px),250px] gap-x-8 gap-y-8 pt-[120px] max-w-[1110px] min-h-screen">
+
+      <main className="pt-[150px] mx-auto w-[90%] md:max-w-[1440px] min-h-screen">
         {page}
         <Side />
       </main>
+
       <Footer />
     </div>
   );
