6751f245585463b9b82d49429bda15b17fed0c76
yoshiaki <yoshiosmile68@gmail.com>
2022-03-17 23:37:51 +0900

    吹き出しコンポーネント処理追加と、リファクタ

--- src/pages/article/_component/Article/Main.tsx
+++ src/pages/article/_component/Article/Main.tsx
@@ -2,23 +2,21 @@
 import type { VFC } from "react";
 import { ImageField } from "src/pages/article/_component/Article/ImageField";
 import { ParagraphField } from "src/pages/article/_component/Article/ParagraphField";
+import { SpeechBubbleField } from "src/pages/article/_component/Article/SpeechBubble";
 import type { NewsResponse } from "src/types/microcms";
 
 export const Main: VFC<{ bodys: NewsResponse["bodys"] }> = (props) => {
   return (
     <div className="space-y-3">
       {props.bodys.map((body) => {
-        if (body.fieldId === "image")
-          return <ImageField key={body.alt} fieldId={body.fieldId} image={body.image} alt={body.alt} />;
+        if (body.fieldId === "image") {
+          return <ImageField key={body.image.url} image={body} />;
+        }
         if (body.fieldId === "paragraph") {
-          return (
-            <ParagraphField
-              key={body.h2_title}
-              fieldId={body.fieldId}
-              h2_title={body.h2_title}
-              h2_content={body.h2_content}
-            />
-          );
+          return <ParagraphField key={body.h2_title} paragraph={body} />;
+        }
+        if (body.fieldId === "speechBubble") {
+          return <SpeechBubbleField key={body.speechBody[0].speechText} bubble={body} />;
         }
       })}
     </div>
