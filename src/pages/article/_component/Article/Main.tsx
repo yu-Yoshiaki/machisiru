0aaa87783105a53f0572c485ff75f414eab6ca84
yoshiaki <yoshiosmile68@gmail.com>
2022-03-18 00:55:24 +0900

    呼び出し短縮

--- src/pages/article/_component/Article/Main.tsx
+++ src/pages/article/_component/Article/Main.tsx
@@ -1,9 +1,10 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import type { VFC } from "react";
-import { ImageField } from "src/pages/article/_component/Article/ImageField";
-import { ParagraphField } from "src/pages/article/_component/Article/ParagraphField";
-import { SpeechBubbleField } from "src/pages/article/_component/Article/SpeechBubble";
 import type { NewsResponse } from "src/types/microcms";
+
+import { ImageField } from "./ImageField";
+import { ParagraphField } from "./ParagraphField";
+import { SpeechBubbleField } from "./SpeechBubbleField";
 
 export const Main: VFC<{ bodys: NewsResponse["bodys"] }> = (props) => {
   return (
