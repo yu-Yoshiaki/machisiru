a8e40dd92dea79122ef0be8714fd016779ce7ff6
yoshiaki <yoshiosmile68@gmail.com>
2022-03-17 23:38:45 +0900

    個別でpropsを取得していたのを、まとめてに変更

--- src/pages/article/_component/Article/ParagraphField.tsx
+++ src/pages/article/_component/Article/ParagraphField.tsx
@@ -2,13 +2,13 @@
 import type { VFC } from "react";
 import type { ParagraphCustomField } from "src/types/microcms";
 
-export const ParagraphField: VFC<ParagraphCustomField> = (props) => {
+export const ParagraphField: VFC<{ paragraph: ParagraphCustomField }> = (props) => {
   return (
     <div className="px-3 space-y-3 w-full">
-      <p className="text-2xl font-bold text-justify break-words md:px-10 md:pt-4 ">{props.h2_title}</p>
+      <p className="text-2xl font-bold text-justify break-words md:px-10 md:pt-4 ">{props.paragraph.h2_title}</p>
       <p
         className="overflow-x-auto p-3 tracking-wide leading-7 text-justify break-words md:p-10"
-        dangerouslySetInnerHTML={{ __html: props.h2_content }}
+        dangerouslySetInnerHTML={{ __html: props.paragraph.h2_content }}
       ></p>
     </div>
   );
