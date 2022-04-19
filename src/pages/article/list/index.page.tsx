1828c86466253ed51720aa17c34b9098d49b81ba
yoshiaki <yoshiosmile68@gmail.com>
2022-04-19 23:43:43 +0900

    編集　レイアウトと新コンポーネント

--- src/pages/article/list/index.page.tsx
+++ src/pages/article/list/index.page.tsx
@@ -2,10 +2,12 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import type { GetStaticProps } from "next";
 import type { CustomNextPage } from "next";
+import Image from "next/image";
 import { useSWRState } from "src/hooks/useSWRState";
 import { Layout } from "src/layout";
 import { client } from "src/libs/microcms";
-import { Card } from "src/pages/article/_component/Card";
+import { New } from "src/pages/article/_component/new";
+import { Recommend } from "src/pages/article/_component/Recommend";
 import type { NewsResponse } from "src/types/microcms";
 
 export const getStaticProps: GetStaticProps = async () => {
@@ -26,26 +28,19 @@ const Index: CustomNextPage<{
 }> = (props) => {
   //一度、静的レンダリングしたデータをSWRでキャッシュ保存
   //他で使い回すため
-  const [blog] = useSWRState("/microcms", props.datas.contents);
+  const [blog]: NewsResponse[][] = useSWRState("/microcms", props.datas.contents);
 
   return (
     <div className="h-full">
-      <div className="grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-4 px-4">
-        {blog ? (
-          blog.map((blog: NewsResponse) => {
-            return (
-              <Card
-                key={blog.id}
-                id={blog.id}
-                updatedAt={blog.updatedAt ? blog.updatedAt.substring(0, 10) : "nothing"}
-                mainTitle={blog.mainTitle}
-                image={blog.mainImage ? blog.mainImage.image.url : "/torii.jpg"}
-              />
-            );
-          })
-        ) : (
-          <h2>undefined</h2>
-        )}
+      <div className="relative">
+        <Image src={"/torii.jpg"} alt={"サービストップ画像"} width={1000} height={800} />
+        <p className="absolute top-[50%] text-xl text-white">僕たちはまだ「日本」を知らない。</p>
+      </div>
+      <h2 className="py-[40px] px-4 text-2xl font-bold">日本の魅力、文化を発信し「暮らし」をアップデートする</h2>
+
+      <div className="space-y-12">
+        {blog ? <Recommend datas={blog} /> : <h2>undefined</h2>}
+        {blog ? <New datas={blog} /> : <h2>undefined</h2>}
       </div>
     </div>
   );
