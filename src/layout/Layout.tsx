0de66b8e60070385988d1b3ff64d7cdc9dde4306
yoshiaki <yoshiosmile68@gmail.com>
2022-04-19 23:35:49 +0900

    スマートフォンサイトに限定するための処理

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,19 +1,26 @@
 import type { CustomLayout } from "next";
+import { useGetWindowSize } from "src/hooks/useGetWindowSizze";
 
 import { Footer } from "./Footer";
 import { Header } from "./Header";
-import { Side } from "./Side";
+import { Profile } from "./Profile";
 
 export const Layout: CustomLayout = (page) => {
+  const { windowSize } = useGetWindowSize();
+
   return (
     <div className="font-mono">
       <Header />
-
-      <main className="lg:grid lg:grid-cols-[1fr,20%] w-full justify-center pt-[100px] mx-auto md:w-[95%] md:max-w-[1440px] min-h-screen">
-        {page}
-        <Side />
-      </main>
-
+      {windowSize.width > 425 ? (
+        <div className="flex items-center justify-center h-screen">
+          <p>すみません。スマートフォン専用サイトになります。PCでの閲覧はできません。</p>
+        </div>
+      ) : (
+        <main className="space-y-4 bg-gray-50 lg:grid lg:grid-cols-[1fr,20%] w-full justify-center pt-[100px] mx-auto md:w-[95%] md:max-w-[1440px] min-h-screen">
+          {page}
+          <Profile />
+        </main>
+      )}{" "}
       <Footer />
     </div>
   );
