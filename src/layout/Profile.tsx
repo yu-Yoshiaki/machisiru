9a26f4917ec6ed8181fb67d8995dc38200c0bef5
yoshiaki <yoshiosmile68@gmail.com>
2022-04-19 23:37:39 +0900

    編集　名前とキャッチコピー

--- src/layout/Profile.tsx
+++ src/layout/Profile.tsx
@@ -7,10 +7,12 @@ export const Profile = () => {
         <Image src="/torii.jpg" width={200} height={200} className="rounded-full" />
       </div>
       <h2 className="p-4 text-xl font-bold text-center">
-        <p>YOSHIAKI</p>
+        <p>MACHISIRU</p>
       </h2>
-      <h3 className="text-sm tracking-wide leading-7 text-left ">
-        FIND NEXT開発者。IT、スポーツ、予防医療など。ジャンルを固定せず、さまざまな情報を発信しいます。
+      <h3 className="text-sm tracking-wide leading-7 text-center">
+        「暮らしをアップデートする」をミッションに、
+        <br />
+        　おすすめの街、地域の魅力をお届けします
       </h3>
     </div>
   );
